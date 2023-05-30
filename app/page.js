2970a1bdf61448eb079da1c48e286cc6e959f06b
David <cleverbearback@gmail.com>
2023-05-29 22:29:14 -0400

    modifying landing page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
 import Image from 'next/image'
+import {BsFillMoonStarsFill} from 'react-icons/bs'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      </div>
+    <main className='bg-white px-10'>
+      <section className='min-h-screen'>
+      <nav className="py-10 mb-12 flex justify-between">
+        <h1>davidcam</h1>
+        <ul className='flex items-center'>
+          <li>
+            <BsFillMoonStarsFill/>
+          </li>
+          <li><a href='#'>Resume</a></li>
+        </ul>
+      </nav>
+      </section>
+      
     </main>
   )
 }
