8351badca6c1f27ab9be09fb62bcf3f0ba41366e
David <cleverbearback@gmail.com>
2023-05-29 22:40:50 -0400

    completing nav

--- app/page.js
+++ app/page.js
@@ -6,12 +6,12 @@ export default function Home() {
     <main className='bg-white px-10'>
       <section className='min-h-screen'>
       <nav className="py-10 mb-12 flex justify-between">
-        <h1>davidcam</h1>
+        <h1 className='text-xl'>davidcam</h1>
         <ul className='flex items-center'>
           <li>
-            <BsFillMoonStarsFill/>
+            <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
           </li>
-          <li><a href='#'>Resume</a></li>
+          <li><a className='bg-yellow-800 text-white px-4 py-2 rounded-md ml-8'href='#'>Resume</a></li>
         </ul>
       </nav>
       </section>
